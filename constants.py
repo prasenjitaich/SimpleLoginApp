be7f1839a5f3800a72d77465de353b53166cf609
Prasenjit Aich <48215957+prasenjitaich@users.noreply.github.com>
2024-01-16 18:25:20 +0530

    Initial Commits

---
+++ constants.py
@@ -0,0 +1,7 @@
+SITE_URL = "http://127.0.0.1:3000"
+LOGIN = '/Login'
+# User
+CREATE_USER_SUCCESS = "User created successfully."
+USERS_GET_SUCCESS = "All users retrieved successfully."
+GET_USER_SUCCESS = "User retrieved successfully."
+USER_DOES_NOT_EXIST = "User does not exist."
